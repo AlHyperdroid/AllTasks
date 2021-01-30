73de37a9fa9f7ae9198ce1dcc9983701bbc4a5dc
AlHyperdroid <corzenoxnikita@gmail.com>
2021-01-30 14:57:50 +0200

    Add second collection and remove one element to MyOwnCollection

--- AllInOne/src/collections/MyOwnCollection.java
+++ AllInOne/src/collections/MyOwnCollection.java
@@ -2,24 +2,44 @@ package collections;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 
 public abstract class MyOwnCollection <E> implements Collection<E> {
 
-public static Collection createMyCollection(){
-    Collection collect = new ArrayList();
-    collect.add("First element");
-    collect.add("Second element");
-    collect.add("Third element");
-    collect.add("Fourth element");
-    collect.add("Fifth element");
-    return collect;
+    public static Collection createMyCollection() {
+        Collection collect = new ArrayList();
+        collect.add("First element");
+        collect.add("Second element");
+        collect.add("Third element");
+        collect.add("Fourth element");
+        collect.add("Fifth element");
+        return collect;
+    }
+
+    public static Collection createSecondCollection() {
+        Collection collecttwo = new ArrayList();
+        return collecttwo;
+    }
+
+    public static void main(String[] args) {
+        Collection col = createMyCollection();
+        for (Object o : col) {
+            System.out.println(o);
+        }
+
+        col.remove("Second element");
+        System.out.println();
+        for (Object o : col) {
+            System.out.println(o);
+        }
+
+        Collection coltwo = createSecondCollection();
+        coltwo.addAll(col);
+        System.out.println();
+        for (Object o : coltwo) {
+            System.out.println(o);
+        }
+    }
 }
 
-public static void main(String[] args) {
-    Collection col = createMyCollection();
-    for(Object o : col){
-        System.out.println(o);
-    }
-    }
-}
