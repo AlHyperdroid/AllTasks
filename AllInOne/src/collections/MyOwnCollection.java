6150331fec4c41fdf6f460e8117cd1cf0d0a41bd
AlHyperdroid <corzenoxnikita@gmail.com>
2021-01-30 13:34:18 +0200

    Add collection to MyOwnCollection

--- AllInOne/src/collections/MyOwnCollection.java
+++ AllInOne/src/collections/MyOwnCollection.java
@@ -1,71 +1,25 @@
 package collections;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
-public class MyOwnCollection <E> implements Collection<E> {
-    @Override
-    public int size() {
-        return 0;
+
+public abstract class MyOwnCollection <E> implements Collection<E> {
+
+public static Collection createMyCollection(){
+    Collection collect = new ArrayList();
+    collect.add("First element");
+    collect.add("Second element");
+    collect.add("Third element");
+    collect.add("Fourth element");
+    collect.add("Fifth element");
+    return collect;
+}
+
+public static void main(String[] args) {
+    Collection col = createMyCollection();
+    for(Object o : col){
+        System.out.println(o);
     }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(E e) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
     }
 }
