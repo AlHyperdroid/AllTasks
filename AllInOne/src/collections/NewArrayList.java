970a9e83e88f7f00a2013a6c575a0008a6b4847d
AlHyperdroid <corzenoxnikita@gmail.com>
2021-02-13 00:18:32 +0200

    Update with new methods

--- AllInOne/src/collections/NewArrayList.java
+++ AllInOne/src/collections/NewArrayList.java
@@ -7,12 +7,44 @@ public class NewArrayList<AR> {
     private Object[] arr = new Object[INIT_SIZE];
     private int pointer = 0;
 
+
     public void add(AR thing){
         if(pointer == arr.length-1)
-            INIT_SIZE = INIT_SIZE * 2;
-
-
+            resize(arr.length * 2);
+        arr[pointer++] = thing;
     }
 
+    public AR getElement (int index){
+        return (AR) arr[index];
+    }
 
+    public int arraySize(){
+        return pointer;
+    }
+
+    private void resize(int newLength) {
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(arr, 0, newArray, 0, pointer);
+        arr = newArray;
+    }
+
+    public void delete (int index){
+        for (int i = index; i < pointer; i++){
+            arr[i] = arr[i+1];
+            arr[pointer] = null;
+            pointer--;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static void main(String[] args) {
+        NewArrayList ar = new NewArrayList();
+        ar.add("1");
+        ar.add("2");
+
+    }
 }
