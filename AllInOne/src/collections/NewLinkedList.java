dc70e51bbfa3fc240cc1dc61459e94b81b2b400d
AlHyperdroid <corzenoxnikita@gmail.com>
2021-02-12 15:15:02 +0200

    Add NewLinkedList to collection package

---
+++ AllInOne/src/collections/NewLinkedList.java
@@ -0,0 +1,56 @@
+package collections;
+
+public class NewLinkedList {
+    class Element{
+        String thing;
+        Element perv;
+        Element next;
+        public Element (String thing){
+            this.thing = thing;
+        }
+    }
+    Element first, last = null;
+
+    public void addElement(String thing){
+        Element newElement = new Element(thing);
+
+        if(first == null){
+            first = last = newElement;
+            first.perv = null;
+            last.next = null;
+
+        }else{
+            last.next = newElement;
+            newElement.perv = last;
+            last = newElement;
+            last.next = null;
+        }
+    }
+    public void printElements() {
+        Element curr = first;
+
+        if (first == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        System.out.println("Nodes:");
+        while(curr != null) {
+            System.out.println(curr.thing + " ");
+            curr = curr.next;
+
+        }
+    }
+    public static void main(String[] args) {
+
+        NewLinkedList linkedList = new NewLinkedList();
+
+        linkedList.addElement("this");
+        linkedList.addElement("is");
+        linkedList.addElement("smth");
+        linkedList.addElement("strange");
+        linkedList.addElement("thing");
+
+        linkedList.printElements();
+    }
+}
