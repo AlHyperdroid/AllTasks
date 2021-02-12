f137acf28e9fdedb41cfbdff5c512e09fe28fe7f
AlHyperdroid <corzenoxnikita@gmail.com>
2021-02-12 19:32:23 +0200

    save (not work)

--- AllInOne/src/collections/NewArrayList.java
+++ AllInOne/src/collections/NewArrayList.java
@@ -1,7 +1,18 @@
 package collections;
+import java.util.Arrays;
 
-public class NewArrayList {
+public class NewArrayList<AR> {
+    private int INIT_SIZE = 10;
+    private final int CUT_RATE = 4;
+    private Object[] arr = new Object[INIT_SIZE];
+    private int pointer = 0;
 
+    public void add(AR thing){
+        if(pointer == arr.length-1)
+            INIT_SIZE = INIT_SIZE * 2;
+
+
+    }
 
 
 }
