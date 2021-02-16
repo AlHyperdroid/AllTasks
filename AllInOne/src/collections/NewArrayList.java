7972ce4e1eff3ecc78061bb7d12d6e8cce7f3bf6
AlHyperdroid <corzenoxnikita@gmail.com>
2021-02-16 11:10:57 +0200

    Add methods "printer", "arrListIsEmpty"

--- AllInOne/src/collections/NewArrayList.java
+++ AllInOne/src/collections/NewArrayList.java
@@ -1,5 +1,6 @@
 package collections;
-import java.util.Arrays;
+
+import java.util.SortedMap;
 
 public class NewArrayList<AR> {
     private int INIT_SIZE = 10;
@@ -18,14 +19,14 @@ public class NewArrayList<AR> {
         return (AR) arr[index];
     }
 
-    public int arraySize(){
-        return pointer;
-    }
-
     private void resize(int newLength) {
         Object[] newArray = new Object[newLength];
         System.arraycopy(arr, 0, newArray, 0, pointer);
         arr = newArray;
+    }
+
+    public int arraySize(){
+        return pointer;
     }
 
     public void delete (int index){
@@ -36,15 +37,41 @@ public class NewArrayList<AR> {
         }
     }
 
-    @Override
-    public String toString() {
-        return super.toString();
+    public void printer() {
+        System.out.println("Content of List : ");
+        for (int i = 0; i < pointer; i++) {
+            System.out.println(arr[i].toString());
+        }
     }
+
+    public void arrListIsEmpty(){
+        if (pointer == 0){
+            System.out.println("array is empty");
+        }else {
+            System.out.println("array have: " + arraySize() + " elements");
+        }
+    }
+
 
     public static void main(String[] args) {
         NewArrayList ar = new NewArrayList();
-        ar.add("1");
-        ar.add("2");
-
+        ar.add("First");
+        ar.add("Second");
+        ar.add("Third");
+        ar.add("Fourth");
+        ar.add("Fifth");
+        ar.add("Sixth");
+        ar.add("Seventh");
+        ar.add("Eighth");
+        ar.add("Ninth");
+        ar.add("Tenth");
+        ar.add("Eleventh");
+        ar.printer();
+        System.out.println("");
+        System.out.println(ar.arraySize());
+        System.out.println("");
+        ar.arrListIsEmpty();
+        System.out.println("");
+        System.out.println(ar.getElement(1));
     }
 }
